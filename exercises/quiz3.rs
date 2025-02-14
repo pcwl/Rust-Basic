0123b5ccdab8b4e9135b3321395c6aecf543d4f3
pcwl <freedompcwl@qq.com>
2025-02-14 13:22:31 +0000

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,35 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// 定义一个 trait，用于处理不同类型的等级
+pub trait Grade {
+    fn to_stringg(&self) -> String;
+}
 
-pub struct ReportCard {
-    pub grade: f32,
+// 实现 Grade trait 对于 f32 类型
+impl Grade for f32 {
+    fn to_stringg(&self) -> String {
+        self.to_string()
+    }
+}
+
+// 实现 Grade trait 对于 &str 类型
+impl Grade for &str {
+    fn to_stringg(&self) -> String {
+        self.to_string()
+    }
+}
+
+pub struct ReportCard<T: Grade> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Grade> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+            &self.student_name, &self.student_age, &self.grade.to_stringg())
     }
 }
 
@@ -52,7 +69,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
